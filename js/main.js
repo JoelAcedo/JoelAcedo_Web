2901a267e444c51433ee6e1597409860a2a77a57
Joel Acedo <joel.acedo@est.fib.upc.edu>
2016-10-25 15:22:46 +0200

    Added profile image, and start to work with stylus

--- js/main.js
+++ js/main.js
@@ -1,24 +1,24 @@
 (function($) {
-  $(document).ready(function() {
+    $(document).ready(function() {
 
-    $("#nav-header").find("a").click(function() {
-      var $href = $(this).attr('href');
-      var $anchor = $("#" + $href).offset();
+        $("#nav-header").find("a").click(function() {
+            var $href = $(this).attr('href');
+            var $anchor = $("#" + $href).offset();
 
-      // window.scrollTo($anchor.left, $anchor.top);
-      $("html, body").animate({
-        scrollTop: $anchor.top
-      }, 1000);
+            // window.scrollTo($anchor.left, $anchor.top);
+            $("html, body").animate({
+                scrollTop: $anchor.top
+            }, 1000);
 
-      return false;
+            return false;
+        });
+
+        // $(window).scroll(function() {
+        //   if ($(this).scrollTop() > $('.page-header').height()) {
+        //     $('#nav-header').fadeIn(300);
+        //   } else {
+        //     $('#nav-header').fadeOut(300);
+        //   }
+        // });
     });
-
-    // $(window).scroll(function() {
-    //   if ($(this).scrollTop() > $('.page-header').height()) {
-    //     $('#nav-header').fadeIn(300);
-    //   } else {
-    //     $('#nav-header').fadeOut(300);
-    //   }
-    // });
-  });
-}) (jQuery);
+})(jQuery);
